4badf4cbe879fb303eceaaec3d2a8518914549b8
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2015-06-08 18:47:48 +0000

    rubocop refactoring for user_finder_spec

--- spec/models/user_finder_spec.rb
+++ spec/models/user_finder_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe 'UserFinder Class' do
-  let (:cas_session) do
-    { 'cas' => {'user'=>'rsnodgra',
-      'ticket'=>'ST-516271-B9XA0vT6ncxmPgzDsUKt-cas',
-      'extra_attributes'=>{}} }
+describe 'UserFinder Class with maunually written session value' do
+  let :cas_session do
+    { 'cas' => { 'user' => 'rsnodgra',
+                 'ticket' => 'ST-516271-B9XA0vT6ncxmPgzDsUKt-cas',
+                 'extra_attributes' => {} } }
   end
-  let (:non_cas_session) do
+  let :non_cas_session do
     Hash.new
   end
   it "assigns 'anonymous' when no cas is detected" do
@@ -17,11 +17,11 @@ describe 'UserFinder Class' do
     i = Guaranteeduser::UserFinder.new(cas_session)
     expect(i.name).to eq('rsnodgra')
   end
-  it 'assigns cas user with one long function call' do 
+  it 'assigns cas user with one long function call' do
     i = Guaranteeduser::UserFinder.new(cas_session).name
     expect(i).to eq('rsnodgra')
   end
-    it "assigns anonymous with long function call without '[:cas]'" do 
+  it "assigns anonymous with long function call without '[:cas]'" do
     i = Guaranteeduser::UserFinder.new(non_cas_session).name
     expect(i).to eq('anonymous')
   end
@@ -29,10 +29,10 @@ end
 RSpec.configure do |c|
   c.infer_base_class_for_anonymous_controllers = false
 end
-RSpec.describe Guaranteeduser::UserFinder, :type => :controller do
+RSpec.describe Guaranteeduser::UserFinder, type: :controller do
   controller do
     def index
-      render :nothing => true
+      render nothing: true
     end
   end
   context 'with full access to true session object' do
@@ -41,7 +41,7 @@ RSpec.describe Guaranteeduser::UserFinder, :type => :controller do
       i = Guaranteeduser::UserFinder.new(session).name
       expect(i).to eq('anonymous')
     end
-    it 'when I manually set a session user it tells me my user name' do
+    it 'and I manually set a session user it tells me my user name' do
       user = create(:user)
       establish_current_user(user)
       get :index
@@ -50,4 +50,3 @@ RSpec.describe Guaranteeduser::UserFinder, :type => :controller do
     end
   end
 end
-
